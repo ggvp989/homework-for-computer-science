4ef359bb51482968cd0da7091486b9d44cbb5ba5
ggvp989 <2000akimvolgin@mail.ru>
2025-11-26 15:18:45 +0700

    Update task_1.py

--- практика 5/task_1.py
+++ практика 5/task_1.py
@@ -1,1 +1,5 @@
-
+def e1(s1, s2):
+    return sorted(s1 + s2);
+sea_fish = ["shark", "flounder", "tuna", "cod", "herring", "Marlin"] 
+freshwater_fish = ["Asp", "Pike", "Carp", "Salmon", "Ide", "Trout"]
+print(e1(sea_fish, freshwater_fish))
