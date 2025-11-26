6998ab20f01a07a1021d98a87f43abd31515eebf
ggvp989 <2000akimvolgin@mail.ru>
2025-11-26 15:20:46 +0700

    Update task_2.py

--- практика 5/task_2.py
+++ практика 5/task_2.py
@@ -1,1 +1,8 @@
-
+for i, flowers in enumerate(flowers_set, 1):
+    n = len(flowers)
+    print(2 ** n - 1)
+flowers_set = [
+    {"rose", "jasmine", "lily"},
+    {"orchid", "tulip", "violet", "daisy"},
+    {"lavender", "sunflower"}
+]
